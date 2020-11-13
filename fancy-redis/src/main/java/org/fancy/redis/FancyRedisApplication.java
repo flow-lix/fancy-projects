708bbe06c0f0797fa1aa2bbb5e35bb6d8f6abe0a
lxiang17 <lxiang17@grgbanking.com>
2020-11-13 10:46:15 +0800

    添加redis

---
+++ fancy-redis/src/main/java/org/fancy/redis/FancyRedisApplication.java
@@ -0,0 +1,13 @@
+package org.fancy.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FancyRedisApplication.class, args);
+    }
+
+}
---
+++ fancy-redis/src/test/java/org/fancy/redis/FancyRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fancy.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancyRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
